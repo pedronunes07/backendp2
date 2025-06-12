268c4e2f075610c0dd4968c3da8d790d921483b5
Pedro Marcos <pedromarcosnunes@gmail.com>
2025-06-12 19:47:54 -0300

    Questão 3

--- resolucaop2/todos/views.py
+++ resolucaop2/todos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Todo
 
 class TodoListView(ListView):
@@ -11,4 +11,10 @@ class TodoCreateView(CreateView):
     model = Todo
     fields = ['title','deadline']
     success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
 
